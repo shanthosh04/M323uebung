71ce1f74f8074d26da8fd4d978d7700320aaa896
shanthosh04 <133089176+shanthosh04@users.noreply.github.com>
2023-08-31 00:05:48 +0200

    Add files via upload

---
+++ M323uebung/javascript/functions.js
@@ -0,0 +1,21 @@
+function mul(number1, number2){
+    const result = number1 * number2;
+    console.log(result);
+}
+
+mul(1,2);
+mul(2,3);
+mul(4,5);
+
+function fahrheit(celsius) {
+    const result = * 1.8+32;
+    console.log('Celsius',celsius, 'Fahrenehit',result);
+}
+
+fahrenheit(80)
+fahrenheit(200)
+
+const modulo = (a,b) => a&b;
+
+console.log(modulo(10,3));
+console.log(modulo(10,5));
